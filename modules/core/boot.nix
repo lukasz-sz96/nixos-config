d44f01bb03c4fad6449b53154002f6e1643e2279
Łukasz <lukasz.szczesny96@gmail.com>
2026-05-29 23:33:35 +0200

    enable amd_pstate

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -2,6 +2,7 @@
 
 {
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
+  boot.kernelParams = [ "amd_pstate=active" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
