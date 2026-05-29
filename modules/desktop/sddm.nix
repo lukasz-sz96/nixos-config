a15091d059e0319f64f9fbf0ce6ffe6b9407de7e
Łukasz <lukasz.szczesny96@gmail.com>
2026-05-29 22:53:53 +0200

    update sddm

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,34 +1,69 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  cursorTheme = "Bibata-Modern-Ice";
+  cursorSize = 24;
+
+  custom-sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "astronaut";
+  };
+in
 {
   services.xserver.enable = false;
   services.greetd.enable = false;
   services.displayManager.gdm.enable = false;
+
+  programs.niri.enable = true;
+  services.displayManager.defaultSession = "niri";
 
   services.displayManager.sddm = {
     enable = true;
     package = pkgs.kdePackages.sddm;
     theme = "sddm-astronaut-theme";
 
-    wayland.enable = true;
+    wayland = {
+      enable = true;
+      compositor = "weston";
+    };
 
-    extraPackages = with pkgs.kdePackages; [
-      qtmultimedia
-      qtsvg
-      qtvirtualkeyboard
+    enableHidpi = true;
+
+    extraPackages = [
+      custom-sddm-astronaut
+      pkgs.bibata-cursors
+
+      pkgs.kdePackages.qtmultimedia
+      pkgs.kdePackages.qtsvg
+      pkgs.kdePackages.qtvirtualkeyboard
     ];
 
     settings = {
-      General.InputMethod = "";
       Theme = {
-        CursorSize = 24;
-        CursorTheme = "Adwaita";
+        Current = "sddm-astronaut-theme";
+        CursorTheme = cursorTheme;
+        CursorSize = cursorSize;
+      };
+
+      General = {
+        DefaultSession = "niri.desktop";
       };
     };
   };
 
   environment.systemPackages = [
+    custom-sddm-astronaut
+    pkgs.bibata-cursors
     pkgs.adwaita-icon-theme
-    pkgs.sddm-astronaut
   ];
+
+  environment.sessionVariables = {
+    XCURSOR_THEME = cursorTheme;
+    XCURSOR_SIZE = toString cursorSize;
+  };
+
+  systemd.services.display-manager.environment = {
+    XCURSOR_THEME = cursorTheme;
+    XCURSOR_SIZE = toString cursorSize;
+    XCURSOR_PATH = "${pkgs.bibata-cursors}/share/icons:/run/current-system/sw/share/icons";
+  };
 }
