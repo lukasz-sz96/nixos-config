7c7033b56cec569989674bf97045c7c12b2234c7
Łukasz <lukasz.szczesny96@gmail.com>
2026-06-02 23:53:14 +0200

    increase terminal padding

--- modules/home/shared/programs/ghostty.nix
+++ modules/home/shared/programs/ghostty.nix
@@ -39,8 +39,8 @@ _:
           font-family = "JetBrainsMono Nerd Font";
           font-size = 13;
           theme = "noctalia";
-          window-padding-x = 10;
-          window-padding-y = 10;
+          window-padding-x = 14;
+          window-padding-y = 14;
         };
       };
 
--- modules/home/shared/programs/kitty.nix
+++ modules/home/shared/programs/kitty.nix
@@ -14,7 +14,7 @@ _:
         confirm_os_window_close = 0;
         cursor_shape = "beam";
         cursor_trail = 1;
-        window_margin_width = 10;
+        window_margin_width = 14;
       };
 
       keybindings = {
